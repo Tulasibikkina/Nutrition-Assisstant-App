18bc3242cbd9dec0f69543b9103ee166feadc104
tulasibikkina <tulasi.bikkina@sasi.ac.in>
2026-07-13 23:32:11 +0530

    Added frontend and backend folders

---
+++ frontend/login.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const login = async () => {
+    const res = await axios.post("http://localhost:5000/api/users/login", {
+      email,
+      password
+    });
+
+    localStorage.setItem("token", res.data.token);
+    window.location.href = "/dashboard";
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <input placeholder="Email" onChange={e => setEmail(e.target.value)} />
+      <input placeholder="Password" type="password"
+             onChange={e => setPassword(e.target.value)} />
+      <button onClick={login}>Login</button>
+    </div>
+  );
+}
+
+export default Login;
